86beca700bd96924e280a0c98c5f8b00b7895ebe
Luis Palencia <luis.palencia525@gmail.com>
2022-01-20 19:42:12 +0100

    [WIP] - Added Utils class -
    + Created Singleton Pattern
    + Created generic function with delegate + datasource

--- 19012022/MiAppPickerView/MiAppPickerView/Sources/SegundaViewController.swift
+++ 19012022/MiAppPickerView/MiAppPickerView/Sources/SegundaViewController.swift
@@ -29,7 +29,74 @@ class SegundaViewController: UIViewController {
     
 
     private func configuracionUI(){
+        self.perfilImageView.image = UIImage(named: self.dataSourceFotosPerfil[0])
+        Utils.shared.configuracionPickerView(tag: 1,
+                                             delegate: self,
+                                             dataSource: self,
+                                             textxFields: self.localidadesTF,
+                                             dataArray: self.dataSourceLocalidades)
         
+        Utils.shared.configuracionPickerView(tag: 2,
+                                             delegate: self,
+                                             dataSource: self,
+                                             textxFields: self.codigoPostalTF,
+                                             dataArray: self.dataSourceCodigosPostales)
+        
+        Utils.shared.configuracionPickerView(tag: 3,
+                                             delegate: self,
+                                             dataSource: self,
+                                             textxFields: self.prioridadTrabajoTF,
+                                             dataArray: self.dataSourcePrioridadTeletrabajo)
     }
 
 }
+
+extension SegundaViewController: UIPickerViewDelegate{
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch pickerView.tag {
+        case 1:
+            return self.dataSourceLocalidades[row]
+        case 2:
+            return self.dataSourceCodigosPostales[row]
+        default:
+            return self.dataSourcePrioridadTeletrabajo[row]
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        50
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch pickerView.tag {
+        case 1:
+            self.localidadesTF.text =  self.dataSourceLocalidades[row]
+        case 2:
+            self.codigoPostalTF.text =  self.dataSourceCodigosPostales[row]
+        default:
+            self.prioridadTrabajoTF.text =  self.dataSourcePrioridadTeletrabajo[row]
+            self.perfilImageView.image = UIImage(named: self.dataSourceFotosPerfil[row])
+        }
+    }
+    
+}
+
+extension SegundaViewController: UIPickerViewDataSource{
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch pickerView.tag {
+        case 1:
+            return self.dataSourceLocalidades.count
+        case 2:
+            return self.dataSourceCodigosPostales.count
+        default:
+            return self.dataSourcePrioridadTeletrabajo.count
+        }
+    }
+}
+
