0ba893c3a50a4830bfb37a7d52d321c6b5eaa8b3
Luis Palencia <luis.palencia525@gmail.com>
2022-01-21 19:05:38 +0100

    [WIP] - Work done on 20-01-2022

---
+++ 20012022/MiDataNavigation/MiDataNavigation/Source/TerceraViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TerceraViewController.swift
+//  MiDataNavigation
+//
+//  Created by CICE on 20/01/2022.
+//
+
+import UIKit
+
+class TerceraViewController: UIViewController {
+    
+    // MARK: - Variables
+    var datosUsuario = DatosUsuarioModel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 20012022/MiDataNavigation/MiDataNavigation/Utils+Helpers/Utils.swift
@@ -0,0 +1,39 @@
+//
+//  Utils.swift
+//  MiDataNavigation
+//
+//  Created by CICE on 20/01/2022.
+//
+
+import Foundation
+import UIKit
+
+
+class Utils {
+    static let shared = Utils()
+    
+    
+    /// method showAlertVC return a alert generic component
+    /// - Parameters:
+    ///   - title: title put from VC to Alert generic component
+    ///   - message: message put from VC to Alert generic component
+    /// - Returns: return UIAlertController component
+    func showAlertVC(title: String, message: String) -> UIAlertController{
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        return alertVC
+    }
+}
+
+
+struct DatosUsuarioModel {
+    var nombreData: String?
+    var apellidoData: String?
+    var telefonoData: String?
+    var direccionData: String?
+    var edadPerroData: String?
+    var codigoPostalData: String?
+    var ciudadData: String?
+    var posicionGeograficaData: String?
+    
+}
