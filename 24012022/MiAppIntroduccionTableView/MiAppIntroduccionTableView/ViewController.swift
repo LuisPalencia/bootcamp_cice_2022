7acbe17216a2c0e6ea907fc8234d3f311c474a78
Luis Palencia <luis.palencia525@gmail.com>
2022-01-24 19:14:26 +0100

    [INIT-PROJECT] - Start new project MiIntroduccion Table View - + With storyboard, + App CoreCoordinator

---
+++ 24012022/MiAppIntroduccionTableView/MiAppIntroduccionTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiAppIntroduccionTableView
+//
+//  Created by CICE on 24/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
