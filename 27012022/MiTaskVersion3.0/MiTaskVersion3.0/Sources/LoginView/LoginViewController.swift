649eb48676cb3e76954cc7aa177f3b50e324c1e5
Luis Palencia <luis.palencia525@gmail.com>
2022-01-31 14:25:36 +0100

    [WIP] - Work done on 28-01-2022

--- 27012022/MiTaskVersion3.0/MiTaskVersion3.0/Sources/LoginView/LoginViewController.swift
+++ 27012022/MiTaskVersion3.0/MiTaskVersion3.0/Sources/LoginView/LoginViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    // MARK: - Variables globales
+    var usuarioLogado = false
+    
     // MARK: - IBOutlets
     
     @IBOutlet weak var usernameTF: UITextField!
@@ -20,44 +23,72 @@ class LoginViewController: UIViewController {
     
     
     @IBAction func rememberMeACTION(_ sender: Any) {
-        
+        if datosCompletados() {
+            self.usuarioLogado = true
+            Utils.Constantes().kPrefs.setValue(self.usernameTF.text, forKey: Utils.Constantes().kUsername)
+            Utils.Constantes().kPrefs.setValue(self.passwordTF.text, forKey: Utils.Constantes().kPassword)
+            Utils.Constantes().kPrefs.setValue(self.usuarioLogado, forKey: Utils.Constantes().kUsuarioLogado)
+            self.configuracionUI(color: #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1))
+        }else{
+            self.present(Utils.muestraAlerta(titulo: "Hey!",
+                                             mensaje: "Rellena el username y el password",
+                                             completionHandler: nil),
+                         animated: true,
+                         completion: nil)
+        }
     }
     
     
     @IBAction func loginACTION(_ sender: Any) {
-        
+        if datosCompletados() {
+            self.borrarDatosFormulario()
+            
+        }else{
+            self.present(Utils.muestraAlerta(titulo: "Hey!",
+                                             mensaje: "Rellena el username y el password",
+                                             completionHandler: nil),
+                         animated: true,
+                         completion: nil)
+        }
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.configuracionUI()
+        self.configuracionUI(color: #colorLiteral(red: 1, green: 0.2823529412, blue: 0.25824927, alpha: 1))
         // Do any additional setup after loading the view.
     }
 
-    private func configuracionUI(){
+    private func configuracionUI(color: UIColor){
         self.contentViewUsername.backgroundColor = .clear
         self.contentViewUsername.layer.cornerRadius = 37
         self.contentViewUsername.layer.borderWidth = 2
-        self.contentViewUsername.layer.borderColor = #colorLiteral(red: 1, green: 0.2827598444, blue: 0.25824927, alpha: 1)
+        self.contentViewUsername.layer.borderColor = color.cgColor
         
         self.contentViewPassword.backgroundColor = .clear
         self.contentViewPassword.layer.cornerRadius = 37
         self.contentViewPassword.layer.borderWidth = 2
-        self.contentViewPassword.layer.borderColor = #colorLiteral(red: 1, green: 0.2827598444, blue: 0.25824927, alpha: 1)
+        self.contentViewPassword.layer.borderColor = color.cgColor
         
         self.loginBTN.layer.cornerRadius = 22
 
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func datosCompletados() -> Bool {
+        return !(self.usernameTF.text?.isEmpty ?? false) && !(self.passwordTF.text?.isEmpty ?? false)
     }
-    */
+    
+    private func borrarDatosFormulario(){
+        self.usernameTF.text = ""
+        self.passwordTF.text = ""
+        self.mostrarHomeTabBar()
+    }
+    
+    private func mostrarHomeTabBar() {
+        let vc = HomeTabBarViewCoordinator.homeViewController()
+        vc.modalTransitionStyle = .coverVertical
+        vc.modalPresentationStyle = .fullScreen
+        self.present(vc, animated: true, completion: nil)
+    }
 
 }
